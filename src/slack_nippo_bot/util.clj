cac6c2b167c89ef8074775202c3def735a58a483
wasuken <wevorence@gmail.com>
2020-02-16 20:13:45 +0900

    update config.

--- src/slack_nippo_bot/util.clj
+++ src/slack_nippo_bot/util.clj
@@ -2,6 +2,8 @@
   (:require [aero.core :refer [read-config]])
   (:gen-class))
 
-(def config (read-config (clojure.java.io/resource "config.edn")))
+(def config
+  (if (.exist (clojure.java.io/resource "config.edn"))
+    (read-config (clojure.java.io/resource "config.edn"))))
 
 (def connection {:api-url "https://slack.com/api" :token (:token config)})
