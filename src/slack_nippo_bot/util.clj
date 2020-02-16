b1696e8ecf21d22c8eb8042affa8a8f6b3655282
wasuken <wevorence@gmail.com>
2020-02-16 21:07:44 +0900

    udpate.

--- src/slack_nippo_bot/util.clj
+++ src/slack_nippo_bot/util.clj
@@ -3,7 +3,7 @@
   (:gen-class))
 
 (def config
-  (if (.exist (clojure.java.io/resource "config.edn"))
+  (if (.exist (clojure.java.io/file "config.edn"))
     (read-config (clojure.java.io/resource "config.edn"))))
 
 (def connection {:api-url "https://slack.com/api" :token (:token config)})
